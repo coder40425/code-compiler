cf13f05d3ee16be12daa860be4f0808042681df7
Yash Kumar Singh <yashsingh1610@gmail.com>
2026-06-11 18:19:42 +0530

    redis added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import { connectRedis } from "./config/redis";
 import app from "./app";
 import connectDB from "./config/db";
 
@@ -8,7 +9,9 @@ const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   await connectDB();
-
+  
+  await connectRedis();
+  
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
